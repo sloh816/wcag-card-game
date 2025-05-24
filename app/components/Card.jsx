9dce034115f98329549a5aff7e1a08f8edb3f8b9
Shannon Loh <shannon.loh816@gmail.com>
2025-05-24 12:03:29 +1000

    modify card design and add more tiles

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,16 +1,28 @@
-const Card = ({ title, link, icon }) => {
+import iagIcon from "@/assets/iag-icon.png";
+
+const Card = ({ title, link, icon, description }) => {
+	const iconSrc = icon || iagIcon.src;
+	const wrapperClasses = link
+		? "hover:border-grapefruit-100 relative transition-all"
+		: "cursor-not-allowed opacity-50 grayscale";
+
 	return (
-		<div className="grid place-items-center py-8 px-4 border border-navy-20 shadow-md rounded-lg w-64 hover:border-grapefruit-100 relative transition-all">
-			<div className="flex flex-col items-center gap-4">
-				{icon && <img src={icon} alt="" className="h-24" />}
-				<p className="heading-3 text-center">
-					<a
-						className="after:content-normal after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0"
-						href={link}
-					>
-						{title}
-					</a>
-				</p>
+		<div
+			className={`py-4 px-6 pb-6 border border-navy-20 shadow-md rounded-lg w-80 h-full ${wrapperClasses}`}
+		>
+			<div className="flex items-start gap-4">
+				{iconSrc && <img src={iconSrc} alt="" className="w-10" />}
+				<div className="flex flex-col gap-2">
+					<p className="heading-3">
+						<a
+							className="after:content-normal after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0"
+							href={link}
+						>
+							{title}
+						</a>
+					</p>
+					{description && <p className="text-charcoal-100 text-sm">{description}</p>}
+				</div>
 			</div>
 		</div>
 	);
--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,91 @@
 "use client";
-import icon from "@/assets/iag-icon.png";
 import Card from "@/components/Card";
+import icon from "@/assets/iag-icon.png";
 
 const HomePage = ({}) => {
 	const databases = [
 		{
-			title: "Easy Read style guide",
-			link: "/style-guide"
+			title: "Easy Read Style Guide",
+			link: "/style-guide",
+			description: "A writing style guide for IAG Easy Read content."
 		},
 		{
-			title: "User testing feedback",
-			link: "/user-testing"
+			title: "User Testing Feedback",
+			link: "/user-testing",
+			description: "User testing feedback collection from our focus group testing."
+		},
+		{
+			title: "Easy Read Database",
+			description: "Search for images and text from all our past Easy Read content."
 		}
 	];
 
 	const tools = [
 		{
-			title: "InDesign to Word (HTML converter)",
-			link: "/indesign-to-word"
+			title: "InDesign to Word (HTML Converter)",
+			link: "/indesign-to-word",
+			description: "Process an InDesign HTML file to copy into a Word document."
+		},
+		{
+			title: "PDF Strip tables",
+			description: "Strip the <Table> tags from the PDF tags. Useful for doing Easy Read FA."
+		},
+		{
+			title: "Easy Read HTML Builder",
+			description: "Generate an Easy Read HTML from a Word or InDesign document."
+		},
+		{
+			title: "Word to HTML Builder",
+			description: "Generate an Easy Read HTML from a Word or InDesign document."
+		},
+		{
+			title: "Easy Read Alt Text Generator",
+			description:
+				"Write alt text for images in a Word document. Alt text suggestions are generated for images used in past Easy Read documents."
+		},
+		{
+			title: "Web Audit Report Generator",
+			description:
+				"Record the issues of your website audit and generate a Word and HTML report."
 		}
 	];
 
 	return (
 		<>
-			<header className="container p-8 flex gap-4 items-center mt-8">
-				<img src={icon.src} alt="IAG Icon" className="w-12" />
+			<header className="px-12 flex gap-4 flex-col items-center my-20 container">
+				<img src={icon.src} alt="IAG Icon" className="w-16" />
 				<h1 className="font-bold text-navy-100 text-3xl">Tools & Databases</h1>
 			</header>
-			<main className="mt-4 container">
+			<main className="px-12 mt-4 container mb-20">
 				<h2 className="heading-2 mb-2">Databases</h2>
 				<ul className="flex gap-4">
 					{databases.map((tool) => {
 						return (
 							<li key={tool.title}>
-								<Card title={tool.title} link={tool.link} icon={icon.src} />
+								<Card
+									title={tool.title}
+									link={tool.link}
+									description={tool.description}
+									icon={tool.icon}
+									disabled={tool.disabled ? tool.disabled : false}
+								/>
 							</li>
 						);
 					})}
 				</ul>
 
 				<h2 className="heading-2 mb-2 mt-16">Tools</h2>
-				<ul className="flex gap-4">
+				<ul className="flex gap-4 flex-wrap">
 					{tools.map((tool) => {
 						return (
 							<li key={tool.title}>
-								<Card title={tool.title} link={tool.link} icon={icon.src} />
+								<Card
+									title={tool.title}
+									link={tool.link}
+									description={tool.description}
+									icon={tool.icon}
+									disabled={tool.disabled ? tool.disabled : false}
+								/>
 							</li>
 						);
 					})}
